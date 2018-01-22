9e0b460ce1d43c63f74c976b981e519ebf38262a
Iain Scott <i.scott@winton.com>
2018-01-22 14:50:37 +0000

    Added TabularConverter.

---
+++ src/Winton.Extensions.Serialization.Json/TabularConverter.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace Winton.Extensions.Serialization.Json
+{
+    public sealed class TabularConverter : JsonConverter
+    {
+        private readonly string _columnsProperty;
+        private readonly string _rowsProperty;
+
+        public TabularConverter()
+            : this("columns")
+        {
+        }
+
+        public TabularConverter(string columnsProperty = "columns", string rowsProperty = "rows")
+        {
+            _columnsProperty = columnsProperty;
+            _rowsProperty = rowsProperty;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(IList).GetTypeInfo().IsAssignableFrom(objectType.GetTypeInfo());
+        }
+
+        public override object ReadJson(
+            JsonReader reader,
+            Type objectType,
+            object existingValue,
+            JsonSerializer serializer)
+        {
+            var columns = new List<string>();
+            var rows = new List<List<object>>();
+
+            if (reader.TokenType != JsonToken.StartObject)
+            {
+                throw new JsonSerializationException("Expects an object.");
+            }
+
+            while (reader.Read() && reader.TokenType == JsonToken.PropertyName)
+            {
+                var name = (string)reader.Value;
+
+                if (name == _columnsProperty)
+                {
+                    if (reader.Read() && reader.TokenType == JsonToken.StartArray)
+                    {
+                        while (reader.Read() && reader.TokenType != JsonToken.EndArray)
+                        {
+                            columns.Add((string)reader.Value);
+                        }
+                    }
+                    else
+                    {
+                        throw new JsonSerializationException(
+                            $"Expects '{_columnsProperty}' property to be an array of strings.");
+                    }
+                }
+                else if (name == _rowsProperty)
+                {
+                    if (reader.Read() && reader.TokenType == JsonToken.StartArray)
+                    {
+                        while (reader.Read() && reader.TokenType != JsonToken.EndArray)
+                        {
+                            if (reader.TokenType == JsonToken.StartArray)
+                            {
+                                var row = new List<object>();
+
+                                while (reader.Read() && reader.TokenType != JsonToken.EndArray)
+                                {
+                                    row.Add(serializer.Deserialize(reader));
+                                }
+
+                                rows.Add(row);
+                            }
+                            else
+                            {
+                                throw new JsonSerializationException(
+                                    $"Expects '{_rowsProperty}' property to be an array of arrays.");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        throw new JsonSerializationException(
+                            $"Expects '{_rowsProperty}' property to be an array of arrays.");
+                    }
+                }
+                else
+                {
+                    throw new JsonSerializationException(
+                        $"Expects an object with properties '{_columnsProperty}' and '{_rowsProperty}'.");
+                }
+            }
+
+            if (reader.TokenType != JsonToken.EndObject)
+            {
+                throw new JsonSerializationException("Object is malformed.");
+            }
+
+            Type type = objectType.GenericTypeArguments.Single();
+            PropertyInfo[] properties = columns
+                .GroupJoin(type.GetTypeInfo().DeclaredProperties, x => x, y => y.Name, (x, y) => y.SingleOrDefault())
+                .ToArray();
+            var list = (IList)Activator.CreateInstance(objectType);
+
+            foreach (List<object> row in rows)
+            {
+                object element = Activator.CreateInstance(type);
+
+                foreach ((PropertyInfo property, object value) in properties.Zip(row, Tuple.Create))
+                {
+                    if (property != null)
+                    {
+                        object typedValue =
+                            value != null && type.GetTypeInfo().IsAssignableFrom(value.GetType().GetTypeInfo())
+                                ? value
+                                : Convert.ChangeType(value, property.PropertyType);
+
+                        property.SetValue(element, typedValue);
+                    }
+                }
+
+                list.Add(element);
+            }
+
+            return list;
+        }
+
+        public override void WriteJson(
+            JsonWriter writer,
+            object value,
+            JsonSerializer serializer)
+        {
+            Type type = value.GetType().GenericTypeArguments.Single();
+            PropertyInfo[] properties = type.GetTypeInfo().DeclaredProperties.ToArray();
+            var list = (IList)value;
+
+            writer.WriteStartObject();
+
+            writer.WritePropertyName(_columnsProperty);
+            writer.WriteStartArray();
+
+            foreach (PropertyInfo property in properties)
+            {
+                writer.WriteValue(property.Name);
+            }
+
+            writer.WriteEndArray();
+
+            writer.WritePropertyName(_rowsProperty);
+            writer.WriteStartArray();
+
+            foreach (object element in list)
+            {
+                writer.WriteStartArray();
+
+                foreach (PropertyInfo property in properties)
+                {
+                    serializer.Serialize(writer, property.GetValue(element));
+                }
+
+                writer.WriteEndArray();
+            }
+
+            writer.WriteEndArray();
+
+            writer.WriteEndObject();
+        }
+    }
+}
---
+++ test/Winton.Extensions.Serialization.Json.Tests/TabularConverterTest.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using FluentAssertions;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Winton.Extensions.Serialization.Json
+{
+    public sealed class TabularConverterTest
+    {
+        [Fact]
+        private void ShouldDeserializeEmpty()
+        {
+            var deserialized = new Container<Item>();
+
+            const string serialized =
+                "{\"columns\":[\"String\",\"Long\",\"Int\",\"Ignore\",\"Double\",\"Decimal\",\"Char\",\"Bool\"],\"rows\":[]}";
+
+            JsonConvert.DeserializeObject<Container<Item>>(serialized).ShouldBeEquivalentTo(deserialized);
+        }
+
+        [Fact]
+        private void ShouldDeserializeSingle()
+        {
+            var deserialized = new Container<Item>
+            {
+                new Item { Bool = true, Char = '2', Int = 3, Long = 4L, Double = 5.6d, Decimal = 7.8m, String = "90" }
+            };
+
+            const string serialized =
+                "{\"columns\":[\"String\",\"Long\",\"Int\",\"Ignore\",\"Double\",\"Decimal\",\"Char\",\"Bool\"],\"rows\":[[\"90\",4,3,\"Ignore\",5.6,7.8,\"2\",true]]}";
+
+            JsonConvert.DeserializeObject<Container<Item>>(serialized).ShouldBeEquivalentTo(deserialized);
+        }
+
+        [Fact]
+        private void ShouldDeserializeWithDefaults()
+        {
+            var deserialized = new Container<Item>
+            {
+                new Item { Bool = false, Int = 3, Double = 5.6d, String = "90" },
+                new Item { Char = '2', Long = 4L, Decimal = 7.8m }
+            };
+
+            const string serialized =
+                "{\"columns\":[\"String\",\"Long\",\"Int\",\"Ignore\",\"Double\",\"Decimal\",\"Char\",\"Bool\"],\"rows\":[[\"90\",0,3,\"Ignore\",5.6,0.0,\"\\u0000\",false],[null,4,0,\"Ignore\",0.0,7.8,\"2\",false]]}";
+
+            JsonConvert.DeserializeObject<Container<Item>>(serialized).ShouldBeEquivalentTo(deserialized);
+        }
+
+        [Fact]
+        private void ShouldSerializeEmpty()
+        {
+            const string serialized =
+                "{\"columns\":[\"Bool\",\"Char\",\"Decimal\",\"Double\",\"Int\",\"Long\",\"String\"],\"rows\":[]}";
+
+            var deserialized = new Container<Item>();
+
+            JsonConvert.SerializeObject(deserialized).ShouldBeEquivalentTo(serialized);
+        }
+
+        [Fact]
+        private void ShouldSerializeSingle()
+        {
+            const string serialized =
+                "{\"columns\":[\"Bool\",\"Char\",\"Decimal\",\"Double\",\"Int\",\"Long\",\"String\"],\"rows\":[[true,\"2\",7.8,5.6,3,4,\"90\"]]}";
+
+            var deserialized = new Container<Item>
+            {
+                new Item { Bool = true, Char = '2', Int = 3, Long = 4L, Double = 5.6d, Decimal = 7.8m, String = "90" }
+            };
+
+            JsonConvert.SerializeObject(deserialized).ShouldBeEquivalentTo(serialized);
+        }
+
+        [Fact]
+        private void ShouldSerializeWithDefaults()
+        {
+            const string serialized =
+                "{\"columns\":[\"Bool\",\"Char\",\"Decimal\",\"Double\",\"Int\",\"Long\",\"String\"],\"rows\":[[false,\"\\u0000\",0.0,5.6,3,0,\"90\"],[false,\"2\",7.8,0.0,0,4,null]]}";
+
+            var deserialized = new Container<Item>
+            {
+                new Item { Bool = false, Int = 3, Double = 5.6d, String = "90" },
+                new Item { Char = '2', Long = 4L, Decimal = 7.8m }
+            };
+
+            JsonConvert.SerializeObject(deserialized).ShouldBeEquivalentTo(serialized);
+        }
+
+        [JsonConverter(typeof(TabularConverter))]
+        private sealed class Container<T> : List<T>
+        {
+        }
+
+        [SuppressMessage(
+            "ReSharper",
+            "UnusedAutoPropertyAccessor.Local",
+            Justification = "Accessors not used but needed implicitly by FluentAssertions ShouldBeEquivalentTo.")]
+        private sealed class Item
+        {
+            public bool Bool { get; set; }
+
+            public char Char { get; set; }
+
+            public decimal Decimal { get; set; }
+
+            public double Double { get; set; }
+
+            public int Int { get; set; }
+
+            public long Long { get; set; }
+
+            public string String { get; set; }
+        }
+    }
+}
